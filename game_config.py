dcb4b65231549ae6fd37248a9b9a158ed5baf1ae
johnny <yanivyo@post.bgu.ac.il>
2021-11-08 11:32:41 +0200

    removed questions

--- game_config.py
+++ game_config.py
@@ -1,12 +1,10 @@
 
 
 class GameConfig(object):
-    def __init__(self, num_of_cars, spawn_rate, obstacle_speed, question_freq, question_duration):
+    def __init__(self, num_of_cars, spawn_rate, obstacle_speed):
         self._num_of_cars = num_of_cars
         self._spawn_rate = spawn_rate
         self._obstacle_speed = obstacle_speed
-        self._question_freq = question_freq
-        self._question_duration = question_duration
 
     @property
     def num_of_cars(self):
@@ -19,11 +17,3 @@ class GameConfig(object):
     @property
     def obstacle_speed(self):
         return self._obstacle_speed
-
-    @property
-    def question_freq(self):
-        return self._question_freq
-
-    @property
-    def question_duration(self):
-        return self._question_duration
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from car import CarLanes
 from car_config import get_car
 from game_config import GameConfig
 from obstacles import Bomb, Star
-from questions import TrueFalseMathQuestion
 
 NUM_OF_CARS = 2
 SCREEN_WIDTH = 800
@@ -22,8 +21,6 @@ DISTANCE_PAST_CAR = MAX_DISTANCE - MAX_DISTANCE * CAR_TO_ROAD_LEN_PROPORTION / 2
 OBSTACLE_INITIAL_DISTANCE = 5
 OBSTACLE_SPEED = 35
 FAILURE_MESSAGE_DISPLAY_TIME = 0.4
-QUESTION_FREQ = 7
-QUESTION_DURATION = 4
 
 
 class Game2Cars(arcade.Window):
@@ -39,13 +36,6 @@ class Game2Cars(arcade.Window):
         self._obstacles = [[] for _ in range(game_config.num_of_cars)]
         self._misses = []
         self._crashes = []
-        self._current_question = None
-        self._last_question_appear_time = 0
-        self._last_question_finish_time = time.time()
-        self._question_appear_times = []
-        self._question_timeouts = []
-        self._question_mistakes = []
-        self._question_successes = []
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
@@ -64,15 +54,7 @@ class Game2Cars(arcade.Window):
             self._draw_car(road_center_x, self._cars[i])
             self._draw_obstacles(road_center_x, self._obstacles[i])
 
-        self._draw_question()
         self._draw_messages()
-
-    def _draw_question(self):
-        if self._current_question is None:
-            return
-        arcade.draw_rectangle_filled(self.width / 2, self.height / 3, self.width / 4, self.height / 10,
-                                     arcade.color.GRAY)
-        arcade.draw_text(str(self._current_question), self.width / 2.6, self.height / 3, font_size=26, bold=True)
 
     def _draw_messages(self):
         last_crash_time = self._crashes[-1] if self._crashes else 0
@@ -81,15 +63,6 @@ class Game2Cars(arcade.Window):
         last_miss_time = self._misses[-1] if self._misses else 0
         if time.time() - last_miss_time < FAILURE_MESSAGE_DISPLAY_TIME:
             arcade.draw_text("Missed star!", self.width / 3, self.height / 2, arcade.color.YELLOW_ORANGE, 32, bold=True)
-        last_question_timeout = self._question_timeouts[-1] if self._question_timeouts else 0
-        if time.time() - last_question_timeout < FAILURE_MESSAGE_DISPLAY_TIME:
-            arcade.draw_text("Too late!", self.width / 2.5, self.height / 3, arcade.color.RED, 32, bold=True)
-        last_question_mistake = self._question_mistakes[-1] if self._question_mistakes else 0
-        if time.time() - last_question_mistake < FAILURE_MESSAGE_DISPLAY_TIME:
-            arcade.draw_text("Wrong!", self.width / 2.5, self.height / 3, arcade.color.RED, 32, bold=True)
-        last_question_success = self._question_successes[-1] if self._question_successes else 0
-        if time.time() - last_question_success < FAILURE_MESSAGE_DISPLAY_TIME:
-            arcade.draw_text("Nice!", self.width / 2.5, self.height / 3, arcade.color.GREEN, 32, bold=True)
 
     def _draw_intro(self):
         arcade.draw_text("Press space to start", self.width * 0.2, self.height / 2, arcade.color.WHITE_SMOKE, 24)
@@ -120,7 +93,6 @@ class Game2Cars(arcade.Window):
         self._obstacle_car_interactions()
         self._remove_finished_obstacles()
         self._spawn_new_obstacles()
-        self._handle_question()
 
     def _obstacle_car_interactions(self):
         for i in range(self._game_config.num_of_cars):
@@ -157,44 +129,12 @@ class Game2Cars(arcade.Window):
                 self._obstacles[i].append(obstacle_type(lane, OBSTACLE_INITIAL_DISTANCE))
                 self._last_spawn_times[i] = time.time()
 
-    def _handle_question(self):
-        if self._current_question is not None and time.time() - self._last_question_appear_time > self._game_config.question_duration:
-            self._handle_question_timeout()
-        if self._current_question is None and time.time() - self._last_question_finish_time > self._game_config.question_freq:
-            self._add_question()
-
-    def _add_question(self):
-        self._current_question = TrueFalseMathQuestion.generate()
-        self._last_question_appear_time = time.time()
-        self._question_appear_times.append(time.time())
-
-    def _handle_question_timeout(self):
-        self._question_timeouts.append(time.time())
-        self._current_question = None
-        self._last_question_finish_time = time.time()
-
     def on_key_press(self, symbol: int, modifiers: int):
         for car in self._cars:
             if symbol in car.keymap:
                 car.lane = car.keymap[symbol]
         if symbol == arcade.key.SPACE and not self._is_started:
             self._start_game()
-        if self._current_question is not None:
-            answer = None
-            if symbol == arcade.key.RALT:
-                answer = True
-            elif symbol == arcade.key.LALT:
-                answer = False
-            if answer is not None:
-                self._handle_user_question_response(answer)
-
-    def _handle_user_question_response(self, answer):
-        if answer == self._current_question.answer:
-            self._question_successes.append(time.time())
-        else:
-            self._question_mistakes.append(time.time())
-        self._current_question = None
-        self._last_question_finish_time = time.time()
 
     def _start_game(self):
         self._is_started = True
@@ -203,15 +143,11 @@ class Game2Cars(arcade.Window):
     def export_game_data(self):
         return json.dumps({"start_time": self._start_time,
                            "crashes": self._crashes,
-                           "misses": self._misses,
-                           "question_appears": self._question_appear_times,
-                           "question_mistakes": self._question_mistakes,
-                           "question_timeouts": self._question_timeouts,
-                           "question_successes": self._question_successes})
+                           "misses": self._misses})
 
 
 def main():
-    config = GameConfig(NUM_OF_CARS, SPAWN_RATE, OBSTACLE_SPEED, QUESTION_FREQ, QUESTION_DURATION)
+    config = GameConfig(NUM_OF_CARS, SPAWN_RATE, OBSTACLE_SPEED)
     game = Game2Cars(SCREEN_WIDTH, SCREEN_HEIGHT, config)
     game.setup()
     arcade.run()
