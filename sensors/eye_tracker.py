bbb74c6ebb383e2d227287f1d9bef10e899ec5d8
johnny <yanivyo@post.bgu.ac.il>
2021-12-23 18:26:23 +0200

    added gaze direction and finalized rounds

--- sensors/eye_tracker.py
+++ sensors/eye_tracker.py
@@ -11,10 +11,11 @@ RECV_BUFFER_SIZE = 1024
 KEEPALIVE_DATA = json.dumps({"op": "start", "type": "live.data.unicast", "key": str(uuid.uuid4())}).encode("ascii")
 KEEPALIVE_FREQ = 2
 PUPIL_DIAMETER_KEY = "pd"
+GAZE_DIRECTION_KEY = "gd"
 EYE_KEY = "eye"
 
 
-def stream_pupil_diameter(callback, timeout=1, smooth_zeros=True):
+def stream_gaze_data(callback, timeout=1, smooth_zeros=True):
     """
     Stream pupil diamater info from Tobii eye tracker to a callback function
 
@@ -29,6 +30,8 @@ def stream_pupil_diameter(callback, timeout=1, smooth_zeros=True):
 
     left_diameter = 0
     right_diameter = 0
+    left_direction = [0, 0, 0]
+    right_direction = [0, 0, 0]
 
     while select.select([sock], [], [], timeout)[0]:
         if time.time() - last_ka_time > KEEPALIVE_FREQ:
@@ -42,14 +45,25 @@ def stream_pupil_diameter(callback, timeout=1, smooth_zeros=True):
                     right_diameter = live_data[PUPIL_DIAMETER_KEY]
                 else:
                     left_diameter = live_data[PUPIL_DIAMETER_KEY]
-            callback(left_diameter, right_diameter)
+        if GAZE_DIRECTION_KEY in live_data:
+            if live_data[EYE_KEY] == "right":
+                right_direction = live_data[GAZE_DIRECTION_KEY]
+            else:
+                left_direction = live_data[GAZE_DIRECTION_KEY]
+        if GAZE_DIRECTION_KEY in live_data or PUPIL_DIAMETER_KEY in live_data:
+            callback(left_diameter, right_diameter, left_direction, right_direction)
 
 
 def stream_to_lsl():
-    info = StreamInfo("Eye tracking", "Gaze", 2)
-    outlet = StreamOutlet(info)
+    pd_info = StreamInfo("Eye tracking - pupil diameter", "Gaze", 2)
+    pd_outlet = StreamOutlet(pd_info)
+    gd_info = StreamInfo("Eye tracking - Gaze direction", "Gaze", 6)
+    gd_outlet = StreamOutlet(gd_info)
 
-    stream_pupil_diameter(lambda left_d, right_d: outlet.push_sample([left_d, right_d]))
+    def callback(left_pd, right_pd, left_gd, right_gd):
+        pd_outlet.push_sample([left_pd, right_pd])
+        gd_outlet.push_sample(left_gd + right_gd)
+    stream_gaze_data(callback)
 
 
 if __name__ == '__main__':
